9c9eaaefffc621306fa06147fbce7d67b82a78e9
sarib ghouri <sarib.ghouri92@gmail.com>
2024-02-24 05:12:07 +0500

    minor fix

--- src/app/cards.js
+++ src/app/cards.js
@@ -6,7 +6,10 @@ import { useEffect, useState } from "react";
 const Cards = () => {
   // const [cardData, setCardData] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const [users, setUsers] = useState(false);
+  const [contractor, setContractor] = useState(false);
+  const [pendingJobs, setPendingJobs] = useState(false);
+  console.log(users)
   const cardData = [
     {
       backgroundImage: "/assets/images/Group.png",
@@ -41,33 +44,28 @@ const Cards = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading(true);
-
         const token = Cookies.get("apiToken");
         const response = await fetch(
-          "https://mksm.blownclouds.com/api/dashboard/cards",
+          "https://doorshark.blownclouds.com/api/adminRoute/getCustomerDetails",
           {
             headers: {
-              "Content-Type": "application/json",
               Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
             },
           }
         );
 
         if (response.ok) {
-          const responseData = await response.json();
-          console.log("Doctors fetched successfully:", responseData);
+          const data = await response.json();
+          console.log("Doctors fetched successfully:", data);
+        
+          setUsers(data.data.length);
 
-          if (Array.isArray(responseData?.data)) {
-            setCardData(responseData.data);
-          } else {
-            console.error(
-              "API response does not contain an array for 'doctor'"
-            );
-          }
+        } else {
+          console.error("Failed to fetch doctors");
         }
       } catch (error) {
-        console.error("Error fetching data: ", error);
+        console.error("Error fetching doctors:", error);
       } finally {
         setLoading(false);
       }
@@ -75,7 +73,102 @@ const Cards = () => {
 
     fetchData();
   }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = Cookies.get("apiToken");
+        const response = await fetch(
+          "https://doorshark.blownclouds.com/api/adminRoute/getContractorDetails",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Doctors fetched successfully:", data);
+        
+          setContractor(data.data.length);
+
+        } else {
+          console.error("Failed to fetch doctors");
+        }
+      } catch (error) {
+        console.error("Error fetching doctors:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = Cookies.get("apiToken");
+        const response = await fetch(
+          "https://doorshark.blownclouds.com/api/adminRoute/gettingJobs",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Doctors fetched successfully:", data);
+        
+          setPendingJobs(data.data.pendingJobs.length);
+
+        } else {
+          console.error("Failed to fetch doctors");
+        }
+      } catch (error) {
+        console.error("Error fetching doctors:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = Cookies.get("apiToken");
+        const response = await fetch(
+          "https://doorshark.blownclouds.com/api/adminRoute/gettingJobs",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Doctors fetched successfully:", data);
+        
+          setPendingJobs(data.data.completedJobs.length);
+
+        } else {
+          console.error("Failed to fetch doctors");
+        }
+      } catch (error) {
+        console.error("Error fetching doctors:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="flex flex-wrap gap-[20px] justify-center w-[100%] mt-[20px]">
       {/* {cardData.map((card, index) => {
@@ -111,12 +204,12 @@ const Cards = () => {
           />
           <p className="text-[#054fb9] w-[60%] p-[8px] font-medium text-[20px] bg-[#fff] rounded-r-[10px] rounded-l-[10px]">
             All Contractor:
-            {/* {card.type} */}
+           
           </p>
           <p className="flex text-white text-[24px] font-bold text-30">
             {/* {card.count} */}
           </p>
-          <p className="flex Receipt font-bold">230</p>
+          <p className="flex Receipt font-bold">{contractor}</p>
         </div>
       </div>
       <div
@@ -143,9 +236,64 @@ const Cards = () => {
           <p className="flex text-white text-[24px] font-bold text-30">
             {/* {card.count} */}
           </p>
-          <p className="flex Receipt font-bold">230</p>
+          <p className="flex Receipt font-bold">{users}</p>
         </div>
       </div>
+      <div
+        className="overflow-hidden w-[100%]  rounded-[20px] doorCards"
+        style={{
+          width: "400px",
+          height: "234px",
+
+          backgroundColor: "rgb(69, 129, 168)", // Updated background color
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="ml-[20px] mt-[25px] relative">
+          <img
+            className=" absolute w-[200px] h-[160px]  right-0 bottom-[-117px] "
+            src="/assets/images/cardGroup.png"
+            alt=""
+          />
+          <p className="text-[#054fb9] w-[60%] p-[8px] font-medium text-[20px] bg-[#fff] rounded-r-[10px] rounded-l-[10px]">
+           Pending Jobs:
+            {/* {card.type} */}
+          </p>
+          <p className="flex text-white text-[24px] font-bold text-30">
+            {/* {card.count} */}
+          </p>
+          <p className="flex Receipt font-bold">{pendingJobs}</p>
+        </div>
+      </div>
+      <div
+        className="overflow-hidden w-[100%]  rounded-[20px] doorCards"
+        style={{
+          width: "400px",
+          height: "234px",
+
+          backgroundColor: "rgb(69, 129, 168)", // Updated background color
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="ml-[20px] mt-[25px] relative">
+          <img
+            className=" absolute w-[200px] h-[160px]  right-0 bottom-[-117px] "
+            src="/assets/images/cardGroup.png"
+            alt=""
+          />
+          <p className="text-[#054fb9] w-[60%] p-[8px] font-medium text-[20px] bg-[#fff] rounded-r-[10px] rounded-l-[10px]">
+             Complete Jobs:
+            {/* {card.type} */}
+          </p>
+          <p className="flex text-white text-[24px] font-bold text-30">
+            {/* {card.count} */}
+          </p>
+          <p className="flex Receipt font-bold">{users}</p>
+        </div>
+      </div>
+      
       {/* ); */}
       {/* })} */}
     </div>
