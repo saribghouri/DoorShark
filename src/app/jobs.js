a716658c4b90bbb8396ca0b71d331919f659d6fa
sarib ghouri <sarib.ghouri92@gmail.com>
2024-02-26 23:25:01 +0500

    minor fix

--- src/app/jobs.js
+++ src/app/jobs.js
@@ -174,7 +174,7 @@ const Jobs = () => {
         <div>
           <div className="flex justify-between  pl-[10px] pr-[10px] ml-[16px] mr-[16px] items-center mt-[20px] mb-[20px]">
             <h1 className="Doctors text-[22px] text-[#054fb9] font-sans">
-              Job
+              Jobs
             </h1>
             <Input
               className="w-[300px] rounded-[40px]"
--- src/app/pendingJobs.js
+++ src/app/pendingJobs.js
@@ -175,7 +175,7 @@ const PendingJobs = () => {
         <div>
           <div className="flex justify-between  pl-[10px] pr-[10px] ml-[16px] mr-[16px] items-center mt-[20px] mb-[20px]">
             <h1 className="Doctors text-[22px] text-[#054fb9] font-sans">
-              Pendindg
+              Pending Jobs
             </h1>
             <Input
               className="w-[300px] rounded-[40px]"
